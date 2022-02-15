402c13a66a40a77f19c2d992f3d8f368c8050d9d
Remco Vermeulen <rverme@ext.uber.com>
2022-02-15 12:44:41 +0100

    Fix await usage

--- cleanup.js
+++ cleanup.js
@@ -1,15 +1,19 @@
 const core = require('@actions/core')
 const io = require('@actions/io')
 
-try {
-  core.info("Running cleanup task.")
-  var workspacePath = process.env['RUNNER_WORKSPACE']
-  if (!workspacePath) {
-    throw new Error('RUNNER_WORKSPACE not defined')
+async function run() {
+  try {
+    core.info("Running cleanup task.")
+    var workspacePath = process.env['RUNNER_WORKSPACE']
+    if (!workspacePath) {
+      throw new Error('RUNNER_WORKSPACE not defined')
+    }
+    core.debug(`RUNNER_WORKSPACE = '${workspacePath}'`)
+    core.info("Removing workspace {workspacePath}")
+    await io.rmRF(workspacePath)
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  core.debug(`RUNNER_WORKSPACE = '${workspacePath}'`)
-  core.info("Removing workspace {workspacePath}")
-  await io.rmRF(workspacePath)
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run()
