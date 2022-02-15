77eebed3859a27bfc0f36370b016a8cb6f65b22b
Remco Vermeulen <rverme@ext.uber.com>
2022-02-15 13:30:02 +0100

    Fix string interpolation

--- cleanup.js
+++ cleanup.js
@@ -3,13 +3,12 @@ const io = require('@actions/io')
 
 async function run() {
   try {
-    core.info("Running cleanup task.")
     var workspacePath = process.env['RUNNER_WORKSPACE']
     if (!workspacePath) {
       throw new Error('RUNNER_WORKSPACE not defined')
     }
     core.debug(`RUNNER_WORKSPACE = '${workspacePath}'`)
-    core.info("Removing workspace {workspacePath}")
+    core.info(`Removing workspace ${workspacePath}`)
     await io.rmRF(workspacePath)
   } catch (error) {
     core.setFailed(error.message);
